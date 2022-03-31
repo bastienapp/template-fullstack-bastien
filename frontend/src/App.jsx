588211ba062768166242897b23bc14642b19cf77
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-31 12:29:31 +0200

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/items")
+      .then(({ data }) => {
+        setItems(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <p>coucou</p>
-    </div>
+    <ul className="App">
+      {items.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ul>
   );
 }
 
